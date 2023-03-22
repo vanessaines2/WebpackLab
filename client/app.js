8ce39a8f9e1e0291e914a0585d3182dc4fcbb921
Vanessaines2 <Vanessaines2@icloud.com>
2023-03-22 11:29:04 -0400

    webpack bundle, and solution

--- client/app.js
+++ client/app.js
@@ -1,19 +1,19 @@
+import { voteTakeout, voteLeftovers, getWinner } from "./score.js";
 const winnerText = document.getElementById("winner");
 const leftoversBtn = document.getElementById("leftovers");
 const takeoutBtn = document.getElementById("takeout");
 
 const updateWinner = () => {
-	const currWinner = getWinner();
-	winnerText.innerText = currWinner;
+  const currWinner = getWinner();
+  winnerText.innerText = currWinner;
 };
 
 takeoutBtn.addEventListener("click", () => {
-	voteTakeout();
-	updateWinner();
+  voteTakeout();
+  updateWinner();
 });
 
 leftoversBtn.addEventListener("click", () => {
-	voteLeftovers();
-	updateWinner();
+  voteLeftovers();
+  updateWinner();
 });
-
--- client/score.js
+++ client/score.js
@@ -1,17 +1,17 @@
 let takeoutScore = 0;
 let leftoversScore = 0;
 
-const getWinner = () => {
-	if (takeoutScore === leftoversScore) return "It's a tie";
-	else {
-		return takeoutScore > leftoversScore ? "Order out" : "Eat Leftovers";
-	}
+export const getWinner = () => {
+  if (takeoutScore === leftoversScore) return "It's a tie";
+  else {
+    return takeoutScore > leftoversScore ? "Order out" : "Eat Leftovers";
+  }
 };
 
-const voteTakeout = () => {
-	takeoutScore++;
+export const voteTakeout = () => {
+  takeoutScore++;
 };
 
-const voteLeftovers = () => {
-	leftoversScore++;
+export const voteLeftovers = () => {
+  leftoversScore++;
 };
